2e1f3161029ac2c2a553b83b6385bbaf66b36f01
EYOSIYAS <eyosiyasmengesha@gmail.com>
2024-07-16 19:55:58 +0300

    added events

--- contract/tweeter.sol
+++ contract/tweeter.sol
@@ -22,6 +22,12 @@ contract Tweeter {
         _;
     }
 
+    event TweetCreated (address author, uint256 id, uint256 timestamp, string content);
+
+    event TweetLiked (address tweetAuthor, address liker, uint256 likeCount, uint256 tweetId);
+
+    event TweetUnliked( address tweetAuthor , address unliker, uint256 likeCount, uint256 tweetId);
+
     function createTweet (string memory _tweet) public {
 
         require(bytes(_tweet).length <= MaxTweetLength, "The tweet is too long");
@@ -36,7 +42,8 @@ contract Tweeter {
         });
         // array of tweets of individual user
         tweet[msg.sender].push(newTweet);
-
+         
+        emit TweetCreated(msg.sender, tweet[msg.sender].length, block.timestamp, _tweet);
     }
 
     function getTweet (address user, uint256 _index) public view returns (Tweets memory){
@@ -54,11 +61,15 @@ contract Tweeter {
     function likeTweet (address _author, uint256 _id) external {
         require(tweet[_author][_id].id == _id, "the tweet doesn't exists");
         tweet[_author][_id].likes++;
+
+        emit TweetLiked(_author, msg.sender, tweet[_author][_id].likes, tweet[_author][_id].id);
     }
     
     function unlikeTweet ( address _author , uint256 _id) external {
         require(tweet[_author][_id].id == _id, "the tweet doesn't exists");
         require(tweet[_author][_id].likes >= _id, "the tweet doen't have likes");
-        tweet[_author][_id].likes++;
+        tweet[_author][_id].likes--;
+
+        emit TweetUnliked(_author, msg.sender,  tweet[_author][_id].likes, _id);
     }
 } 
